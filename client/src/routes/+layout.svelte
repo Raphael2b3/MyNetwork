bdec0c8e92e963116910df1d7dd924f00edb783e
Raphael Schütz <raphael.schuetz0311@gmail.com>
2023-05-25 00:03:40 +0200

    first steps

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>
+
+<style>
+    *{
+        display:flex;
+        background-color: rgb(85, 170, 227);
+        align-items: center;
+    }
+
+</style>
---
+++ client/src/routes/settings/+page.svelte
@@ -0,0 +1,1 @@
+Bro i dont know how to do that
